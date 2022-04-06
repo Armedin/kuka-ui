3167bbc90c2e6a7752d9b7d71da2538b3525c2ea
Armedin Kuka <armedinkuka@Armedins-MacBook-Air.local>
2022-04-06 05:11:40 +0100

    Small changes

--- packages/system/src/styledSx/styledSx.d.ts
+++ packages/system/src/styledSx/styledSx.d.ts
@@ -1,2 +1,41 @@
 declare const styledSx: (props: any) => any;
 export default styledSx;
+
+import * as CSS from 'csstype';
+
+import { OverwriteCSSProperties } from './OverwriteCSSProperties';
+
+export type ResponsiveStyleValue<T> =
+  | T
+  | Array<T | null>
+  | { [key: string]: T | null };
+
+export type CSSPseudoSelectorProps<Theme extends object = {}> = {
+  [K in CSS.Pseudos]?: SystemStyleObject<Theme>;
+};
+
+export interface CSSSelectorObject<Theme extends object = {}> {
+  [cssSelector: string]: SystemStyleObject<Theme>;
+}
+
+export interface AllSystemCSSProperties
+  extends Omit<
+      CSS.PropertiesFallback<number | string>,
+      keyof OverwriteCSSProperties
+    >,
+    OverwriteCSSProperties {}
+
+export type SystemCssProperties<Theme extends object = {}> = {
+  [K in keyof AllSystemCSSProperties]:
+    | ResponsiveStyleValue<AllSystemCSSProperties[K]>
+    | ((theme: Theme) => ResponsiveStyleValue<AllSystemCSSProperties[K]>)
+    | SystemStyleObject<Theme>;
+};
+
+export type SystemStyleObject<Theme extends object = {}> =
+  | SystemCssProperties<Theme>
+  | CSSPseudoSelectorProps<Theme>
+  | CSSSelectorObject<Theme>
+  | null;
+
+export type SxProps<Theme extends object = {}> = SystemStyleObject<Theme>;
